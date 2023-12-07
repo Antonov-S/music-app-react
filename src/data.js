fc5f03cc9585abff9f52e74053f9e2638696dfcd
Antonov-S <anto1nov1@gmail.com>
2023-12-07 23:13:31 +0200

    Adding some songs data, new favicon and README

--- src/data.js
+++ src/data.js
@@ -30,29 +30,11 @@ function chillHop() {
       active: false
     },
     {
-      name: "Nightfall",
-      cover: "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
-      artist: "Aiguille",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9148",
-      color: ["#EF8EA9", "#ab417f"],
-      id: uuidv4(),
-      active: false
-    },
-    {
       name: "Reflection",
       cover: "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
       artist: "Swørn",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=9228",
       color: ["#CD607D", "#c94043"],
-      id: uuidv4(),
-      active: false
-    },
-    {
-      name: "Under the City Stars",
-      cover: "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
-      artist: "Aso, Middle School, Aviino",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=10074",
-      color: ["#205950", "#2ab3bf"],
       id: uuidv4(),
       active: false
     },
@@ -80,6 +62,312 @@ function chillHop() {
       artist: "The Doppelgangaz",
       audio: "https://stream.chillhop.com/mp3/68326",
       color: ["#9EABBD", "#281D39"],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: "Lost Memories",
+      cover: "https://assets-global.website-files.com/5fc4bca9bd65e8f2bf87f576/6567215adc2e3d05782d52a9_C%2011.webp",
+      artist: "Ruck P, ØDYSSEE, Bastien Brison",
+      audio: "https://stream.chillhop.com/mp3/65377",
+      color: ["#D7E3F5", "#B8D0F1"],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: "Bloom",
+      cover: "https://chillhop.com/wp-content/uploads/2023/07/4a08568d9bf1df419e1baeac2d73e8dd6c517473-1024x1024.jpg",
+      artist: "Leavv, Midan",
+      audio: "https://stream.chillhop.com/mp3/65377",
+      color: ["#F3F5FD", "#716A41"],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: "River's Bass",
+      cover: "https://assets-global.website-files.com/5fc4bca9bd65e8f2bf87f576/64f9ccbdf6a4f372ef8228b3_C%2011.webp",
+      artist: "Vhsceral",
+      audio: "https://stream.chillhop.com/mp3/62122",
+      color: ["#F9F9F9", "#FED763"],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: "Roadtrips",
+      cover: "https://chillhop.com/wp-content/uploads/2023/08/65f57918cd8ef03d204365b8e735f418b3196469-1024x1024.jpg",
+      artist: "Arbour",
+      audio: "https://stream.chillhop.com/mp3/62379",
+      color: ["#628D83", "#11191C"],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: "Beautiful",
+      cover: "https://chillhop.com/wp-content/uploads/2023/06/9180a7fabdc07fe2e221bb26bdd477cb66f6b69e-1024x1024.jpg",
+      artist: "C Y G N",
+      audio: "https://stream.chillhop.com/mp3/60571",
+      color: ["#3F3327", "#C83A36"],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: "Offroad",
+      cover: "https://chillhop.com/wp-content/uploads/2023/05/9e6aaa4e04b9e4054f8ed2586d331113e82247da-1024x1024.jpg",
+      artist: "J.Folk",
+      audio: "https://stream.chillhop.com/mp3/58837",
+      color: ["#C0A582", "#54392B"],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: "Inhale/Ad Astra",
+      cover: "https://chillhop.com/wp-content/uploads/2023/09/7c0628588f526e9948f21027903e146430db4b82-1024x1024.jpg",
+      artist: "A D M B, Boukas",
+      audio: "https://stream.chillhop.com/mp3/55235",
+      color: ["#E67903", "#EFEAE3"],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: "Tomorrow",
+      cover: "https://chillhop.com/wp-content/uploads/2023/08/bcee02e9e81482f5b3579be3796170f7e2a9be2e-1024x1024.jpg",
+      artist: "Hanz",
+      audio: "https://stream.chillhop.com/mp3/62171",
+      color: ["#106C8B", "#021E50"],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: "Pub - Remix by Flitz&Suppe",
+      cover: "https://chillhop.com/wp-content/uploads/2023/08/3f5eb74c8d8e36a38baa2f71be53ea5761ed3c6d-1024x1024.jpg",
+      artist: "Flitz&Suppe",
+      audio: "https://stream.chillhop.com/mp3/43784",
+      color: ["#FCD7AB", "#FD4E51"],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: "Take it Eazy",
+      cover: "https://chillhop.com/wp-content/uploads/2023/06/077e7c815c60119b2d48586aef12c406f4f525a1-1024x1024.jpg",
+      artist: "C Y G N",
+      audio: "https://stream.chillhop.com/mp3/60617",
+      color: ["#A47878", "#764630"],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: "It's Okay",
+      cover: "https://chillhop.com/wp-content/uploads/2023/05/5683fa8ae05221c0d06eeedd90fe00e653d27c48-1024x1024.jpg",
+      artist: "Yasper",
+      audio: "https://stream.chillhop.com/mp3/55163",
+      color: ["#FEF0A1", "#25191B"],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: "Delicate",
+      cover: "https://chillhop.com/wp-content/uploads/2023/02/ef337196272ee56d2e76383d39ed446b57472aec-1024x1024.jpg",
+      artist: "Middle School, Henry Gritton",
+      audio: "https://stream.chillhop.com/mp3/49381",
+      color: ["#FAF7DF", "#8A7E37"],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: "Moving on",
+      cover: "https://chillhop.com/wp-content/uploads/2022/10/0b99c64a1316ecfb990c546ba4a73f6de27cfde5-1024x1024.jpg",
+      artist: "Nymano, Suuna",
+      audio: "https://stream.chillhop.com/mp3/45154",
+      color: ["#D3CDF1", "#4D9CDD"],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: "Lookaway",
+      cover: "https://chillhop.com/wp-content/uploads/2022/09/370fcf77d4ba59808c1109403c537e0fcc0b4edd-1024x1024.jpg",
+      artist: "ØDYSSEE, Blumen",
+      audio: "https://stream.chillhop.com/mp3/44900",
+      color: ["#C1B49E", "#C49900"],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: "No Problem",
+      cover: "https://chillhop.com/wp-content/uploads/2022/11/379d68b8db2ba38393c379a5cdf74ff8b255ae9a-1024x1024.jpg",
+      artist: "cocabona, Misha, J.Lamotta",
+      audio: "https://stream.chillhop.com/mp3/42051",
+      color: ["#CFC7BA", "#1D3165"],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: "Sleep Walker",
+      cover: "https://chillhop.com/wp-content/uploads/2022/08/113a9b73235cab31646ef0059864aec70e793dd2-1024x1024.jpg",
+      artist: "J.Folk",
+      audio: "https://stream.chillhop.com/mp3/43902",
+      color: ["#E5E4B1", "#5C8C72"],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: "Making a Way",
+      cover: "https://chillhop.com/wp-content/uploads/2022/07/e435a424c2a637c19d2241dea2399a4813d984b3-1024x1024.jpg",
+      artist: "The BREED",
+      audio: "https://stream.chillhop.com/mp3/41956",
+      color: ["#CBBDA7", "#B74631"],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: "A Reminder",
+      cover: "https://chillhop.com/wp-content/uploads/2023/01/4a42f136486d42e4d021c673540885c82a1934e6-1024x1024.jpg",
+      artist: "Sleepy Fish, coa",
+      audio: "https://stream.chillhop.com/mp3/36925",
+      color: ["#EEEDB5", "#A40F0B"],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: "Passing Storm",
+      cover: "https://chillhop.com/wp-content/uploads/2023/01/dddab0f5dcf8d0cc3e6276b6d8bff791795a2499-1024x1024.jpg",
+      artist: "Thomas Prime, Moderator",
+      audio: "https://stream.chillhop.com/mp3/42056",
+      color: ["#2C3530", "#875033"],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: "Home",
+      cover: "https://chillhop.com/wp-content/uploads/2022/07/e4292fd3128da3eb44c528641e120ab67be129a9-1024x1024.jpg",
+      artist: "Idealism, SwuM",
+      audio: "https://stream.chillhop.com/mp3/43679",
+      color: ["#EEEEEE", "#1B0E0A"],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: "Everyday",
+      cover: "https://chillhop.com/wp-content/uploads/2022/07/973ae6f7b847fbfe9e2f6a2efbf2a9121ac8285f-1024x1024.jpg",
+      artist: "Aves",
+      audio: "https://stream.chillhop.com/mp3/41890",
+      color: ["#F3E98B", "#5B2E29"],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: "The Field From Spirited Away",
+      cover: "https://chillhop.com/wp-content/uploads/2022/10/1c9e8f008e40a1d5c496a85b2aaf8c96a14a3c88-1024x1024.jpg",
+      artist: "Sleepy Fish, coa",
+      audio: "https://stream.chillhop.com/mp3/36919",
+      color: ["#E8A55C", "#5B0D0D"],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: "In Minutes",
+      cover: "https://chillhop.com/wp-content/uploads/2022/07/49bc1653b33bd42e570ecdeba7be86e8bd1b5c38-1024x1024.jpg",
+      artist: "Shrimpnose",
+      audio: "https://stream.chillhop.com/mp3/41689",
+      color: ["#F4DCCE", "#E5AFD3"],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: "After Hours",
+      cover: "https://chillhop.com/wp-content/uploads/2022/10/36fcc61f2d1c707e99f36706700d73229e2e8dff-1024x1024.jpg",
+      artist: "Poldoore",
+      audio: "https://stream.chillhop.com/mp3/36942",
+      color: ["#E3D5BA", "#C69401"],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: "Falling Out of Orbit",
+      cover: "https://chillhop.com/wp-content/uploads/2023/11/312458b974ab45c795d75ec3f0259caa3989103a-1024x1024.jpg",
+      artist: "Knowmadic",
+      audio: "https://stream.chillhop.com/mp3/41656",
+      color: ["#F3BEC2", "#B05153"],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: "Humility Piece",
+      cover: "https://chillhop.com/wp-content/uploads/2022/09/ce636ede9d5fc9fc2417ce40af98060fbcbdadca-1024x1024.jpg",
+      artist: "Psalm Trees",
+      audio: "https://stream.chillhop.com/mp3/36905",
+      color: ["#DCCEB3", "#0E4370"],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: "Nightfall",
+      cover: "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+      artist: "Aiguille",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9148",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: "Highs and Lows",
+      cover: "https://chillhop.com/wp-content/uploads/2022/09/9bb8c31852704fe0177b21ee1f674b71027b268f-1024x1024.jpg",
+      artist: "Kissamilé",
+      audio: "https://stream.chillhop.com/mp3/36944",
+      color: ["#DACEB6", "#9CB2AD"],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: "Sundials",
+      cover: "https://chillhop.com/wp-content/uploads/2022/07/4f05da370f675eb78a6b92880f93c4e7787935e3-1024x1024.jpg",
+      artist: "chief., Shrimpnose",
+      audio: "https://stream.chillhop.com/mp3/41687",
+      color: ["#FBF7EC", "#D79EC1"],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: "Blessed",
+      cover: "https://chillhop.com/wp-content/uploads/2023/09/73f23095038f6f023083520706f3e5345f5dd682-1024x1024.jpg",
+      artist: "Moods, Yasper",
+      audio: "https://stream.chillhop.com/mp3/32860",
+      color: ["#62AFD6", "#EE7BAF"],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: "Kokon",
+      cover: "https://chillhop.com/wp-content/uploads/2022/05/d8344de22563af8eaec8f544c14711592eabef26-1024x1024.jpg",
+      artist: "Plusma, Guillaume Muschalle",
+      audio: "https://stream.chillhop.com/mp3/28895",
+      color: ["#FFF4C0", "#133042"],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: "Lose Her Way",
+      cover: "https://chillhop.com/wp-content/uploads/2023/11/8cc8bdc59b30dd1f9d401cb485b8537d909e1f50-1024x1024.jpg",
+      artist: "C Y G N",
+      audio: "https://stream.chillhop.com/mp3/31589",
+      color: ["#F3E3E4", "#187F56"],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: "Letters",
+      cover: "https://chillhop.com/wp-content/uploads/2021/12/754c5a4edf9d3bb338dfe89062760f22b7aaec70-1024x1024.jpg",
+      artist: "Oh Caroline, Lhasa Petik, Towerz",
+      audio: "https://stream.chillhop.com/mp3/30131",
+      color: ["#16344A", "#F17250"],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: "Hung Up",
+      cover: "https://chillhop.com/wp-content/uploads/2021/09/dc49d7971512be0192848d072efdaaab91a4c73c-1024x1024.jpg",
+      artist: "Ezzy",
+      audio: "https://stream.chillhop.com/mp3/24699",
+      color: ["#F9DEA9", "#D44847"],
       id: uuidv4(),
       active: false
     }
